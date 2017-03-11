c09a0a2531ebb26b044faa0936dc9f524e37ebe8
Quoc Vuong <daoquocvuong@gmail.com>
2017-03-11 16:16:28 +0700

    Finished server api

--- global.js
+++ global.js
@@ -1,4 +1,4 @@
 module.exports = {
-  providerURL: "",
-  providerToken: ""
+  providerURL: '',
+  providerCookie: '',
 };
--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var sendSMS = require('../lib/provider.js');
 
-/* GET users listing. */
+/* POST query to provider . */
 router.post('/', function(req, res, next) {
-  console.log(req.body)
-  res.send('respond with a resource');
-});
+  if(req.body && req.body.number && req.body.content){
+    let number = req.body.number, content = req.body.content;
 
+    /*Just for VN phone number, mark this block code to commentline if you want to send to other country SMS*/
+    if(number[0]!=0){
+      return res.json({"info": "Phone number must start with 0", "status": "failed", "code": 0});
+    }else{
+      number = "84"+number.substr(1); //"84" is phone number in VN
+    }
+    /*Just for VN phone number, mark this block code to commentline if you want to send to other country SMS*/
+    sendSMS(number, content, (err, body)=>{
+      if(err){
+        return res.json({"info": err, "status": "failed","code": 0});
+      }
+      else if(!err&&body){
+        return res.json({"info": body, "status": "success", "code": 1});
+      }
+    });
+
+  }else{
+    return res.json({"info": "Missing data", "status": "failed", "code": 0});
+  }
+});
 
 module.exports = router;
